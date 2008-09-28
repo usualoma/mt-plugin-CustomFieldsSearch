2266b8f95f8e0a9b4d1a5fde117d15695512f482
usualoma <usualoma@93f3d772-ae55-0410-9c6e-cfb52b349630>
2008-09-28 02:38:14 +0000

    git-svn-id: https://customfieldssearch-mtplugin.googlecode.com/svn/trunk@12 93f3d772-ae55-0410-9c6e-cfb52b349630

---
+++ lib/CustomFieldsSearch/L10N/ja.pm
@@ -0,0 +1,12 @@
+package CustomFieldsSearch::L10N::ja;
+
+use strict;
+use base 'CustomFieldsSearch::L10N::en_us';
+use vars qw( %Lexicon );
+
+%Lexicon = (
+	'toi-planning' => 'ToI企画',
+	'A plugin enable to search by CustromFields data' => 'カスタムフィールドのデータをキーにして検索を行います。',
+);
+
+1;
